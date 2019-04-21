a1bd43ea43f7cae8896a0431207636a4e0cc13c1
theParadox42 <nathaniel.fargo@gmail.com>
2019-04-21 20:20:09 +0000

    changed some files

--- MoreExpress/EJSDemo/index.js
+++ MoreExpress/EJSDemo/index.js
@@ -1,19 +1,19 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // Paths
 app.get("/", function(req, res){
-    res.render("home")
-})
+    res.render("home");
+});
 app.get("/fallinlove/:name", function(req, res){
     var name = req.params.name;
     res.render("love", {name: name});
 });
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server started")
-})
+app.listen(8000, undefined, function(){
+    console.log("Server started");
+});
