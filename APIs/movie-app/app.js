89014d708515428f57dfa7ba38e5dce1c158c8a8
theParadox42 <nathaniel.fargo@gmail.com>
2019-04-22 23:00:17 +0000

    Wow

--- APIs/movie-app/app.js
+++ APIs/movie-app/app.js
@@ -5,13 +5,15 @@ var request = require("request");
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.redirect("/results");
+	res.render("search");
 });
 app.get("/results", function(req, res){
 	request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
 		if(!error && response.statusCode == 200){
-			var results = JSON.parse(body);
-			res.send(results.Search[0].Title);
+			var data = JSON.parse(body);
+			res.render("results", {data: data});
+		} else {
+			res.send("error fetching data");
 		}
 	});
 });
