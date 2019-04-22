3abed3f4865d167c10eee11f0cc2126c51113199
theParadox42 <nathaniel.fargo@gmail.com>
2019-04-22 22:38:29 +0000

    removed and added some files

--- APIs/movie-app/app.js
+++ APIs/movie-app/app.js
@@ -2,8 +2,18 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.redirect("/results");
+});
 app.get("/results", function(req, res){
-	res.send("it works!");
+	request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var results = JSON.parse(body);
+			res.send(results.Search[0].Title);
+		}
+	});
 });
 
 app.listen(9000, undefined, function(){
