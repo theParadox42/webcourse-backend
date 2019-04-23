c3fddf8c5753710c6618521625f6e34946d45e5d
theParadox42 <nathaniel.fargo@gmail.com>
2019-04-23 00:02:52 +0000

    did some initial yelpcamp stuff

--- APIs/movie-app/app.js
+++ APIs/movie-app/app.js
@@ -8,7 +8,10 @@ app.get("/", function(req, res){
 	res.render("search");
 });
 app.get("/results", function(req, res){
-	request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+	var searchQuery = req.query.search;
+	console.log(searchQuery);
+	var url = "http://omdbapi.com/?s=" + searchQuery + "&apikey=thewdb";
+	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200){
 			var data = JSON.parse(body);
 			res.render("results", {data: data});
---
+++ YelpCamp/app.js
@@ -0,0 +1,34 @@
+// Setup
+var express = require("express");
+var app = express();
+
+
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+app.get("/campgrounds", function(req, res){
+	//Campgrounds
+	var campgrounds = [
+		{
+			name: "First Campground",
+			img: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"
+		},
+		{
+			name: "Granite Hill",
+			img: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"
+		},
+		{
+			name: "Happy River",
+			img: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"
+		},
+	];
+	
+	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.listen(process.env.PORT || 9000, process.env.IP || undefined, function(){
+	console.log("Yelp Camp server has started!");
+});
