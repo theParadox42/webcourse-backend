7006a7c69720d341253e027b261456ebb3aa13a2
Paradox Programming <nathaniel.fargo@gmail.com>
2019-04-23 15:47:23 +0000

    More yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,31 +7,39 @@ var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// DATA
+
+//Campgrounds
+var campgrounds = [
+	{
+		name: "First Campground",
+		img: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"
+	},
+	{
+		name: "Granite Hill",
+		img: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"
+	},
+	{
+		name: "Happy River",
+		img: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"
+	},
+];
+
 // ROUTES
 app.get("/", function(req, res){
 	res.render("landing");
 });
 app.get("/campgrounds", function(req, res){
-	//Campgrounds
-	var campgrounds = [
-		{
-			name: "First Campground",
-			img: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"
-		},
-		{
-			name: "Granite Hill",
-			img: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"
-		},
-		{
-			name: "Happy River",
-			img: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"
-		},
-	];
 	
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 app.post("/campgrounds", function(req, res){
-	console.log("requested campground");
+	var newCampground = req.body;
+	
+	campgrounds.push(newCampground);
+	
+	res.redirect("/campgrounds");
+	
 });
 app.get("/campgrounds/new", function(req, res){
 	res.render("new");
@@ -39,6 +47,6 @@ app.get("/campgrounds/new", function(req, res){
 
 
 // RUN APP
-app.listen(process.env.PORT || 9000, process.env.IP || undefined, function(){
+app.listen(process.env.PORT || 9000, process.env.IP, function(){
 	console.log("Yelp Camp server has started!");
 });
