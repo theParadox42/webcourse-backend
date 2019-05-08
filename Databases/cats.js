d400851bd21167079b57f0934a2a16653d79e0ef
theParadox42 <nathaniel.fargo@gmail.com>
2019-05-08 00:11:45 +0000

    cats

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,12 +1,14 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/cat_app', { useNewUrlParser: true }); 
+mongoose.connect('mongodb://localhost:27017/cat_app', { useNewUrlParser: true }).catch(function(e){
+	console.log(e);
+}); 
 
 
 var catSchema = new mongoose.Schema({
     name: String,
     age: Number,
     cuteness: Number
-})
+});
 
 var Cat = mongoose.model("Cat", catSchema);
 
@@ -16,14 +18,14 @@ var george = new Cat({
     name: "George",
     age: 50,
     cuteness: 0.1,
-})
+});
 
 george.save(function(err, cat){
     if (err) {
-        console.log("Something went wrong adding a cat")
+        console.log("Something went wrong adding a cat");
     } else {
-        console.log("Cat added")
-        console.log(cat)
+        console.log("Cat added");
+        console.log(cat);
     }
 });
 
