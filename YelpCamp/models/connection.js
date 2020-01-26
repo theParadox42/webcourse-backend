10a7fe3df74d8f3470c6d906f143886c8ab37f77
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-01-25 17:15:02 -0700

    Ummm... YelpCamp Changes

--- YelpCamp/models/connection.js
+++
@@ -1,5 +0,0 @@
-// require('dotenv').config();
-
-var Env = process.env;
-module.exports = 'mongodb+srv://'
-    + Env.DB_USER + ':' + Env.DB_PASS + '@data-sodyq.mongodb.net/' + Env.DB + '?retryWrites=true&w=majority'
--- YelpCamp/models/sendJSON.js
+++
@@ -1,9 +0,0 @@
-
-function sendJSON(res, object, type){
-    res.json({
-        data: object,
-        type: type
-    })
-}
-
-module.exports = sendJSON;
